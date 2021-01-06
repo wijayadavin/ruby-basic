c8b3aff52a7c77b5f460c69ade6f64f2e2c1aec8
Davin Wijaya <wijayadavin@gmail.com>
2021-01-06 09:49:56 +0700

    Finished CRUD articles

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,50 @@
 module Api
     module V1
         class ArticlesController < ApplicationController
+            # Root:
             def index
                 articles = Article.order('created_at DESC');
                 render json: {status: 'SUCCESS', message:'Loaded articles', data: articles}, status: :ok
+            end
+
+            # Get article by ID:
+            def show
+                article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message:'Loaded article', data: article}, status: :ok
+            end
+
+            # Create article:
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: {status: 'SUCCESS', message:'Saved article', data: article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message:'Failed to save article', data: article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            # Delete article by ID:
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {status: 'SUCCESS', message:'The article was successfully deleted', data: article}, status: :ok
+            end
+
+            # Edit article by ID:
+            def update
+                article = Article.find(params[:id])
+                if article.update(article_params)
+                    render json: {status: 'SUCCESS', message:'Successfully update the article', data: article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message:'Failed to update the article', data: article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def article_params
+                params.permit(:title, :body)
             end
         end
     end
